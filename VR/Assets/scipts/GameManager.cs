dcc1e4c269d95864ecbff742733da95ec78188a3
lisa971210 <61856088+lisa971210@users.noreply.github.com>
2020-03-27 11:55:25 +0800

    0327

--- VR/Assets/scipts/GameManager.cs
+++ VR/Assets/scipts/GameManager.cs
@@ -1,6 +1,44 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-public GameObject lightgroup;
+    [Header("燈光群組")]
+    public GameObject lightgroup;
+    [Header("抽屜")]
+    public Transform drawer;
+    [Header("門")]
+    public Transform door;
+    public IEnumerator LightEffect()
+    {
+        lightgroup.SetActive(false);
+        yield return new WaitForSeconds(0.2f);
+        lightgroup.SetActive(true);
+        yield return new WaitForSeconds(0.3f);
+        lightgroup.SetActive(false);
+        yield return new WaitForSeconds(0.1f);
+        lightgroup.SetActive(true);
+        yield return new WaitForSeconds(0.2f);
+        lightgroup.SetActive(false);
+        yield return new WaitForSeconds(0.15f);
+        lightgroup.SetActive(true);
+    }
+    public void StartObjectMove()
+    {
+        StartCoroutine(ObjectMove());
+    }
+    public IEnumerator ObjectMove()
+    {
+        for (int i = 0; i < 30; i++)
+        {
+            drawer.position -= drawer.forward*0.1f;
+            yield return new WaitForSeconds(0.01f);
+        }
+        drawer.GetComponent<CapsuleCollider>().enabled = false;
+    }
+
+    private void Start()
+    {
+        StartCoroutine(LightEffect());
+    }
 }
