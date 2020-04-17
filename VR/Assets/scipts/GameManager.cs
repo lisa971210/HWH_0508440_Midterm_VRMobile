7160d5f15822efa4b91ee5f0b526eb3c19d110a2
lisa971210 <61856088+lisa971210@users.noreply.github.com>
2020-04-17 11:38:06 +0800

    HWH_0508440_Midterm_VRMobile

--- VR/Assets/scipts/GameManager.cs
+++ VR/Assets/scipts/GameManager.cs
@@ -7,6 +7,8 @@ public class GameManager : MonoBehaviour
     public GameObject lightgroup;
     [Header("抽屜")]
     public Transform drawer;
+    [Header("燈柱")]
+    public Transform pillar;
     [Header("喇叭")]
     public AudioSource aud;
     [Header("木板上滑動音效")]
@@ -15,9 +17,23 @@ public class GameManager : MonoBehaviour
     public AudioClip metalsound;
     [Header("敲門音效")]
     public AudioClip doorsound;
+    [Header("敲門音效")]
+    public AudioClip wooddropsound;
     [Header("開門音效")]
     public AudioClip opensound;
-    public Animator anidoor,anishelf,anidrawer, aniclothe, aniclothe2,anifly, anifly2,anicolor;
+    [Header("開")]
+    public AudioClip opensou;
+    [Header("關")]
+    public AudioClip closesou;
+    [Header("木製拉出_開")]
+    public AudioClip draweropensou;
+    [Header("木製拉出_關")]
+    public AudioClip drawerclosesou;
+    [Header("搖")]
+    public AudioClip shakesound;
+    [Header("叮")]
+    public AudioClip dingsound;
+    public Animator anidoor,anishelf,anidrawer, aniclothe, aniclothe2,anifly,anicolor;
     private int countdoor, countshelf, countdrawer, countclothe,countclothe2,countcolor;
 
     public void Lookdoor()
@@ -37,18 +53,21 @@ public class GameManager : MonoBehaviour
         countshelf++;
         if (countshelf == 1)
         {
-            aud.PlayOneShot(doorsound, 5);
+            aud.PlayOneShot(shakesound, 5);
             anishelf.SetTrigger("動");
         }
         if (countshelf == 2)
         {
-            aud.PlayOneShot(doorsound, 5);
+            aud.PlayOneShot(shakesound, 5);
             anishelf.SetTrigger("動");
         }
         else if (countshelf == 3)
         {
-            aud.PlayOneShot(opensound, 4.5f);
+            aud.PlayOneShot(wooddropsound, 4.5f);
             anishelf.SetTrigger("掉");
+        }
+        else if (countshelf == 4)
+        {
         }
     }
     public void Draweroac()
@@ -56,14 +75,14 @@ public class GameManager : MonoBehaviour
         countdrawer++;
         if (countdrawer == 1)
         {
-            aud.PlayOneShot(doorsound, 5);
+            aud.PlayOneShot(draweropensou, 5);
             anidrawer.SetTrigger("開");
  
 
         }
         else if (countdrawer == 2)
         {
-            aud.PlayOneShot(opensound, 4.5f);
+            aud.PlayOneShot(drawerclosesou, 4.5f);
             anidrawer.SetTrigger("關");
 
         }
@@ -74,12 +93,12 @@ public class GameManager : MonoBehaviour
         countclothe2++;
         if (countclothe2 == 1)
         {
-            aud.PlayOneShot(doorsound, 5);
+            aud.PlayOneShot(opensou, 5);
             aniclothe2.SetTrigger("開");
         }
         else if (countclothe2 == 2)
         {
-            aud.PlayOneShot(opensound, 4.5f);
+            aud.PlayOneShot(closesou, 4.5f);
             aniclothe2.SetTrigger("關");
             countclothe2= 0;
         }
@@ -90,43 +109,43 @@ public class GameManager : MonoBehaviour
         countclothe++;
         if (countclothe == 1)
         {
-            aud.PlayOneShot(doorsound, 5);
+            aud.PlayOneShot(opensou, 5);
             aniclothe.SetTrigger("開");
         }
         else if (countclothe == 2)
         {
-            aud.PlayOneShot(opensound, 4.5f);
+            aud.PlayOneShot(closesou, 4.5f);
             aniclothe.SetTrigger("關");
             countclothe = 0;
         }
     }
     public void Bedfly()
     {
-        aud.PlayOneShot(doorsound, 5);
         anifly.Play("fly");
-    }
-    public void Bedfly2()
-    {
-        aud.PlayOneShot(doorsound, 5);
-        anifly2.Play("fly");
     }
     public void Changecolor()
     {
         countcolor++;
         if (countcolor == 1)
         {
-            aud.PlayOneShot(doorsound, 5);
+            aud.PlayOneShot(dingsound, 4);
             anicolor.SetTrigger("a");
         }
         else if (countcolor == 2)
         {
-            aud.PlayOneShot(opensound, 4.5f);
+            aud.PlayOneShot(dingsound, 4f);
             anicolor.SetTrigger("b");
         }
         else if (countcolor == 3)
         {
-            aud.PlayOneShot(opensound, 4.5f);
+            aud.PlayOneShot(dingsound, 4f);
             anicolor.SetTrigger("c");
+        }
+        else if (countcolor == 4)
+        {
+            aud.PlayOneShot(dingsound, 4f);
+            anicolor.SetTrigger("d");
+            countcolor = 1;
         }
 
     }
@@ -148,6 +167,10 @@ public class GameManager : MonoBehaviour
     {
         StartCoroutine(ObjectMove());
     }
+    public void StartObjectMove2()
+    {
+        StartCoroutine(ObjectMove2());
+    }
     public IEnumerator ObjectMove()
     {
         drawer.GetComponent<CapsuleCollider>().enabled = false;
@@ -159,7 +182,17 @@ public class GameManager : MonoBehaviour
 
         aud.PlayOneShot(woodsound, 2.5f);
     }
+    public IEnumerator ObjectMove2()
+    {
+        pillar.GetComponent<CapsuleCollider>().enabled = false;
+        for (int i = 0; i < 30; i++)
+        {
+            pillar.position -= pillar.forward * 0.1f;
+            yield return new WaitForSeconds(0.01f);
+        }
 
+        aud.PlayOneShot(woodsound, 2.5f);
+    }
     private void Start()
     {
         StartCoroutine(LightEffect());
